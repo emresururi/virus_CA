65309ed19994b6000ad6c2c44d630c169912f1b3
O.T <73748939+oguzhantopcu0@users.noreply.github.com>
2020-11-30 16:36:32 +0300

    generate n moving cell
    It creates n moving cells and when the cells touch each other, their states decrease by 1

--- __main__.py
+++ __main__.py
@@ -1,128 +1,114 @@
 from HexGrid import *
 import random
-import pygame as pg
-from time import sleep
+
 nx = 10
 ny = 10
+nx_lim_l, nx_lim_r = -nx/2, nx/2
+ny_lim_l, ny_lim_r = -ny/2, ny/2
+nx_list = []
+ny_list = []
+for i in range(nx + 1):
+    n1 = -nx/2 + i
+    nx_list.append(int(n1))
+for i in range(ny + 1):
+    n = -ny/2 + i
+    ny_list.append(int(n))
 
 # Origin (in terms of i,j)
 oi = 3
 oj = 2
-running = True
+
+grid_bl = HexGrid(nx, ny, "center", (oi, oj))
+
+class cell(object):
+    def __init__(self):
+
+        self.x = random.randint(nx_lim_l, nx_lim_r)
+        self.y = random.randint(ny_lim_l, ny_lim_r)
+        self.state = 9
+        self.death_state = 0
+        self.state_change = 1
+        self.pos = [self.x, self.y]
+    # hareketi tanımlayan fonksiyon
+    def move(self):
+        self.Life = True
+        self.komsular = grid_bl.get_neighbours(self.x, self.y)
+        gidecegi_yer = random.choice(self.komsular)
+        #komsu hucrelerinden birinin statei 2 den buyukse yani yakınlarında başka hucre varsa kendi stateini 1 azaltıyor
+        for i in self.komsular:
+            if grid_bl.get_xy(i[0], i[1]).state >= 2:
+                print("temas")
+                self.state -= 1
+            if grid_bl.get_xy(self.x, self.y).state == 2:
+                grid_bl.get_xy(self.x, self.y).state = self.death_state
+                self.Life = False
+                print("hücre öldü")
+
+        #gidecegi yerde bir hücre varsa oraya gitmiyor
+        if grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state >= 2 and self.state >= 2:
+            #self.state -= 1
+            self.komsular.remove(gidecegi_yer)
+            new_pos = random.choice(self.komsular)
+            print("yasak konum silindi ve yeni konum seçildi")
+            print("yeni konum", new_pos)
+            grid_bl.get_xy(new_pos[0], new_pos[1]).state = grid_bl.get_xy(self.x, self.y).state
+            grid_bl.get_xy(new_pos[0], new_pos[1]).state = self.state
+            grid_bl.get_xy(self.x, self.y).state = 0
+            self.x = new_pos[0]
+            self.y = new_pos[1]
+            self.komsular.append(gidecegi_yer)
+
+        # state 2 den buyukse ve baska etken yoksa normal random hareket ediyor
+        elif self.state >= 2:
+            grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state = grid_bl.get_xy(self.x, self.y).state
+            grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state = self.state
+            grid_bl.get_xy(self.x, self.y).state = 0
+            self.x = gidecegi_yer[0]
+            self.y = gidecegi_yer[1]
+        #eger hucrenin state i 2 ye esit olduysa hucreyi olduruyor
+        if grid_bl.get_xy(self.x, self.y).state == 2:
+            grid_bl.get_xy(self.x, self.y).state = self.death_state
+            self.Life = False
+            print("hücre öldü")
+        grid_bl.visualize()
 
 
-# Create a "centralized" origin at (oi,oj), nx-cell wide x ny-cell high grid
-grid_bl = HexGrid(nx, ny, "center",(oi,oj))
-
-
-# List the neighbours of the cell at <1,1>
-# (since this is a 'bl' grid, it is the "up 1, right one" to the origin
-# at the bottom-left)
-print("Here are the list of neighbours of <0,0>:")
-print(grid_bl.get_neighbours(0, 0))
-
-# Get its state:
-print("The state of the <0,0> cell is: {:}".format(grid_bl.get_xy(0, 0).state))
-
-# Set its state: 3 ayrı state 3 ayrı hasta
-grid_bl.get_xy(0, 0).state = 4
-grid_bl.get_xy(3, 2).state = 5
 
 
 
 
-print("After the update, the state of the <1,1> cell is now: {:}".format(grid_bl.get_xy(1, 1).state))
-
-# Set its neighbours' states:
-#for xy in grid_bl.get_neighbours(0, 0):
-    # print(xy)
-    #grid_bl.get_xy(xy[0], xy[1]).state = 2
-
-#for xy in grid_bl.get_neighbours(5, 0):
-    # print(xy)
-    #grid_bl.get_xy(xy[0], xy[1]).state = 3
 
 
 
+#n tane hücre oluşturma
+cells = []
+n = 5
+for i in range(n):
+    Cell = cell()
+    cells.append(Cell)
 
-# Print the states:
-for x in grid_bl.range_x:
-    for y in grid_bl.range_y:
-        print("{:d},{:d}: {:d}".format(x, y, grid_bl.get_xy(x, y).state), end=" | ")
-    print("")
-
-print(grid_bl.get_xy(0, 0).state)
-mevcut_x1 = 0
-mevcut_y1 = 0
-mevcut_x2 = 3
-mevcut_y2 = 2
-
-
-
-for adimlar in range(10):
-
-    orijinal_koordinatlar_x = mevcut_x1
-    orijinal_koordinat_y = mevcut_y1
-    komsular = grid_bl.get_neighbours(mevcut_x1, mevcut_y1)
-    komsular2 = grid_bl.get_neighbours(mevcut_x2, mevcut_y2)
-    #infected_area = grid_bl.get_xy(mevcut_x2, mevcut_y2).state = 2
-    gidecegi_yer = random.choice(komsular)
-    gidecegi_yer2 = random.choice(komsular2)
-    grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state = grid_bl.get_xy(mevcut_x1, mevcut_y1).state
-    grid_bl.get_xy(gidecegi_yer2[0], gidecegi_yer2[1]).state = grid_bl.get_xy(mevcut_x2, mevcut_y2).state
-    grid_bl.get_xy(mevcut_x2, mevcut_y2).state = 0
-    grid_bl.get_xy(mevcut_x1, mevcut_y1).state = 0
-    mevcut_x1 = gidecegi_yer[0]
-    mevcut_y1 = gidecegi_yer[1]
-    mevcut_x2 = gidecegi_yer2[0]
-    mevcut_y2 = gidecegi_yer2[1]
-    if mevcut_x1 == mevcut_x2 and mevcut_y1 == mevcut_y2 + 1:
-        print("temas")
+#hücrelerin hareketini güncelliyor ve olen hucreleri siliyor
+Running = True
+turn = 0
+while Running:
+    turn += 1
+    print("tur",turn)
+    if len(cells) == 1:
+        print("Simulasyon", turn, ".turda tamamlandı")
         break
-
-    if mevcut_x1 == mevcut_x2 + 1 and mevcut_y1 == mevcut_y2:
-        print("temas")
-        break
-
-    if mevcut_x1 == mevcut_x2 - 1 and mevcut_y1 == mevcut_y2:
-        print("temas")
-        break
-
-    if mevcut_x1 == mevcut_x2 - 1 and mevcut_y1 == mevcut_y2 - 1:
-        print("temas")
-        break
-
-    if mevcut_x1 == mevcut_x2 and mevcut_y1 == mevcut_y2 - 1:
-        print("temas")
-        break
-
-    if mevcut_x1 == mevcut_x2 + 1 and mevcut_y1 == mevcut_y2 - 1:
-        break
-
-    sleep(0.1)
-    print(adimlar)
-    grid_bl.visualize()
-
-xy_hucre1 = [3,3]
-xy_hucre2 = [3,-7]
-n = grid_bl.get_neighbours(xy_hucre1[0],xy_hucre1[1])
-xy = xy_hucre2
-for i in n:
-    var = False
-    if(xy == i ):
-        var = True
-        break
-if(var):
-    print("var")
-else:
-    print("yok")
+    for i in cells:
+        i.move()
+        if i.Life == False:
+            cells.remove(i)
 
 
-
-
-
-
-
+    #state i 1 e esıt olan tum hucrelerin stateini sıfırlıyor arka planı net görebilmek için yaptım
+    for i in nx_list:
+        for j in ny_list:
+            if grid_bl.get_xy(i, j).state == 1:
+                grid_bl.get_xy(i, j).state -= 1
+                print("şu konumda hasta hücre var",[i, j])
+                break
 
 
 
