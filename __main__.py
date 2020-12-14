dd3c6db403f55161f119a0838666b58cbdff22c3
O.T <73748939+oguzhantopcu0@users.noreply.github.com>
2020-12-14 15:47:23 +0300

    Probabilistic arrangements
    Probabilistic illness, dying and recovery in Hexgrid Virus simulation

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 from HexGrid import *
 import random
-
+import time
 nx = 10
 ny = 10
 nx_lim_l, nx_lim_r = -nx/2, nx/2
@@ -20,29 +20,32 @@ oj = 2
 
 grid_bl = HexGrid(nx, ny, "center", (oi, oj))
 
-class cell(object):
+class viruscell(object):
     def __init__(self):
 
         self.x = random.randint(nx_lim_l, nx_lim_r)
         self.y = random.randint(ny_lim_l, ny_lim_r)
-        self.state = 9
+        self.state = 3
         self.death_state = 0
-        self.state_change = 1
+        self.disease = True
         self.pos = [self.x, self.y]
+        self.touch = 0
     # hareketi tanımlayan fonksiyon
-    def move(self):
-        self.Life = True
+    def movevirus(self):
+        self.Lifevirus = True
         self.komsular = grid_bl.get_neighbours(self.x, self.y)
         gidecegi_yer = random.choice(self.komsular)
-        #komsu hucrelerinden birinin statei 2 den buyukse yani yakınlarında başka hucre varsa kendi stateini 1 azaltıyor
+
         for i in self.komsular:
             if grid_bl.get_xy(i[0], i[1]).state >= 2:
-                print("temas")
-                self.state -= 1
-            if grid_bl.get_xy(self.x, self.y).state == 2:
-                grid_bl.get_xy(self.x, self.y).state = self.death_state
-                self.Life = False
-                print("hücre öldü")
+                self.touch += 1
+                #print("viruslu temas")
+                if self.touch == 20:
+                    print("yarasayı yiyen öldü")
+                    grid_bl.get_xy(self.x, self.y).state = self.death_state
+                    self.state = self.death_state
+                #self.Lifevirus = False
+                #print("hücre öldü")
 
         #gidecegi yerde bir hücre varsa oraya gitmiyor
         if grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state >= 2 and self.state >= 2:
@@ -65,18 +68,113 @@ class cell(object):
             grid_bl.get_xy(self.x, self.y).state = 0
             self.x = gidecegi_yer[0]
             self.y = gidecegi_yer[1]
+
+        #state i  0 olduğu zaman hayat durumunu false yapıyor
+        if self.state == self.death_state:
+            grid_bl.get_xy(self.x, self.y).state = self.death_state
+            self.Lifevirus = False
+            print("hücre öldü")
+
+class cell(object):
+    def __init__(self):
+
+        self.x = random.randint(nx_lim_l, nx_lim_r)
+        self.y = random.randint(ny_lim_l, ny_lim_r)
+        self.possibility = [1, 2]
+        self.pdc = ["statusquo1","statusquo2","statusquo3", "d","statusquo4""","statusquo4" ]
+        self.live_or_death = ["L1", "L1", "L1", "L1", "D"]
+        self.state = 4
+        self.sick_state = 3
+        self.deathly_sick_state = 5
+        self.death_state = 0
+        self.state_change = 1
+        self.pos = [self.x, self.y]
+        self.sick_possibility = 2
+        self.deathly_sick_posibility = "healthy"
+        self.live_death = "Live"
+        self.disease = False
+        self.deathly = False
+        self.duration_of_illnes = random.randint(10, 20)
+    # hareketi tanımlayan fonksiyon
+    def move(self):
+        self.Life = True
+        self.komsular = grid_bl.get_neighbours(self.x, self.y)
+        gidecegi_yer = random.choice(self.komsular)
+        #komsu hucrelerinden birinin statei 2 den buyukse yani yakınlarında başka hucre varsa kendi stateini 1 azaltıyor
+        #for i in self.komsular:
+            #if grid_bl.get_xy(i[0], i[1]).state == 3 and self.state == 4:
+                #self.sick_possibility = random.choice(self.possibility)
+
+        if grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state == 3 and self.state == 4:
+            self.sick_possibility = random.choice(self.possibility)
+
+        #gidecegi yerde bir hücre varsa oraya gitmiyor
+        if grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state >= 2:
+            #self.state -= 1
+            self.komsular.remove(gidecegi_yer)
+            new_pos = random.choice(self.komsular)
+            #print("yasak konum silindi ve yeni konum seçildi")
+            #print("yeni konum", new_pos)
+            grid_bl.get_xy(new_pos[0], new_pos[1]).state = grid_bl.get_xy(self.x, self.y).state
+            grid_bl.get_xy(new_pos[0], new_pos[1]).state = self.state
+            grid_bl.get_xy(self.x, self.y).state = 0
+            self.x = new_pos[0]
+            self.y = new_pos[1]
+            self.komsular.append(gidecegi_yer)
+
+        # state 2 den buyukse ve baska etken yoksa normal random hareket ediyor
+        elif self.state >= 2:
+            grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state = grid_bl.get_xy(self.x, self.y).state
+            grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state = self.state
+            grid_bl.get_xy(self.x, self.y).state = 0
+            self.x = gidecegi_yer[0]
+            self.y = gidecegi_yer[1]
         #eger hucrenin state i 2 ye esit olduysa hucreyi olduruyor
         if grid_bl.get_xy(self.x, self.y).state == 2:
             grid_bl.get_xy(self.x, self.y).state = self.death_state
             self.Life = False
             print("hücre öldü")
 
+        if self.sick_possibility == 1:
+            print("Hastalandı")
+            self.disease = True
+            self.deathly_sick_posibility = random.choice(self.pdc)
+            if self.state == 4:
+                self.state = self.sick_state
+
+
+
+            if self.deathly_sick_posibility == "d":
+                print("ölümcül durumda")
+                self.deathly = True
+                self.state = self.deathly_sick_state
+                self.live_death = random.choice(self.live_or_death)
+                if self.live_death == "D":
+                    grid_bl.get_xy(self.x, self.y).state = self.death_state
+                    self.state = self.death_state
+                    self.Life = False
+                    print("hücre öldü")
+
+        if self.disease == True:
+            self.duration_of_illnes -= 1
+            if self.duration_of_illnes == 0:
+                self.disease = False
+                #self.sick_possibility = 0
+                #self.deathly_sick_posibility = 0
+                self.state = 3
+                print("iyilesti")
 
 
 
 
 
 
+
+Viruscells = []
+vn = 3
+for i in range(vn):
+    Virus = viruscell()
+    Viruscells.append(Virus)
 
 
 
@@ -93,14 +191,21 @@ Running = True
 turn = 0
 while Running:
     turn += 1
-    print("tur",turn)
+    #print("tur",turn)
     if len(cells) == 1:
         print("Simulasyon", turn, ".turda tamamlandı")
-        break
+        #break
     for i in cells:
         i.move()
+        #time.sleep(0.1)
         if i.Life == False:
             cells.remove(i)
+
+    for i in Viruscells:
+        i.movevirus()
+        #time.sleep(0.1)
+        if i.Lifevirus == False:
+            Viruscells.remove(i)
     grid_bl.visualize()
 
 
