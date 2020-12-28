eeffe5eff103a51ba3d9ff600cc25dd3403ac514
O.T <73748939+oguzhantopcu0@users.noreply.github.com>
2020-12-28 21:25:12 +0300

    Revised infection , indoor and outdoor possibilty
    The probability of being infected has been adjusted. Possibilities of getting viruses from the point of contact have been arranged in indoor and outdoor areas. The desired number of patient cells were added to the simulation.

--- __main__.py
+++ __main__.py
@@ -1,8 +1,10 @@
 from HexGrid import *
 import random
-import time
+from matplotlib import pyplot as plt
+#import time
 nx = 10
 ny = 10
+
 nx_lim_l, nx_lim_r = -nx/2, nx/2
 ny_lim_l, ny_lim_r = -ny/2, ny/2
 nx_list = []
@@ -20,96 +22,61 @@ oj = 2
 
 grid_bl = HexGrid(nx, ny, "center", (oi, oj))
 
-class viruscell(object):
-    def __init__(self):
 
-        self.x = random.randint(nx_lim_l, nx_lim_r)
-        self.y = random.randint(ny_lim_l, ny_lim_r)
-        self.state = 3
-        self.death_state = 0
-        self.disease = True
-        self.pos = [self.x, self.y]
-        self.touch = 0
-    # hareketi tanımlayan fonksiyon
-    def movevirus(self):
-        self.Lifevirus = True
-        self.komsular = grid_bl.get_neighbours(self.x, self.y)
-        gidecegi_yer = random.choice(self.komsular)
+#self.x = random.randint(nx_lim_l, nx_lim_r)
+#self.y = random.randint(ny_lim_l, ny_lim_r)
 
-        for i in self.komsular:
-            if grid_bl.get_xy(i[0], i[1]).state >= 2:
-                self.touch += 1
-                #print("viruslu temas")
-                if self.touch == 20:
-                    print("yarasayı yiyen öldü")
-                    grid_bl.get_xy(self.x, self.y).state = self.death_state
-                    self.state = self.death_state
-                #self.Lifevirus = False
-                #print("hücre öldü")
 
-        #gidecegi yerde bir hücre varsa oraya gitmiyor
-        if grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state >= 2 and self.state >= 2:
-            #self.state -= 1
-            self.komsular.remove(gidecegi_yer)
-            new_pos = random.choice(self.komsular)
-            print("yasak konum silindi ve yeni konum seçildi")
-            print("yeni konum", new_pos)
-            grid_bl.get_xy(new_pos[0], new_pos[1]).state = grid_bl.get_xy(self.x, self.y).state
-            grid_bl.get_xy(new_pos[0], new_pos[1]).state = self.state
-            grid_bl.get_xy(self.x, self.y).state = 0
-            self.x = new_pos[0]
-            self.y = new_pos[1]
-            self.komsular.append(gidecegi_yer)
 
-        # state 2 den buyukse ve baska etken yoksa normal random hareket ediyor
-        elif self.state >= 2:
-            grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state = grid_bl.get_xy(self.x, self.y).state
-            grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state = self.state
-            grid_bl.get_xy(self.x, self.y).state = 0
-            self.x = gidecegi_yer[0]
-            self.y = gidecegi_yer[1]
+#yatay duvar üreteci
 
-        #state i  0 olduğu zaman hayat durumunu false yapıyor
-        if self.state == self.death_state:
-            grid_bl.get_xy(self.x, self.y).state = self.death_state
-            self.Lifevirus = False
-            print("hücre öldü")
 
 class cell(object):
     def __init__(self):
 
         self.x = random.randint(nx_lim_l, nx_lim_r)
         self.y = random.randint(ny_lim_l, ny_lim_r)
-        self.possibility = [1, 2]
-        self.pdc = ["statusquo1","statusquo2","statusquo3", "d","statusquo4""","statusquo4" ]
+        self.possibility = [1, 2, 3, 4]
+        self.possibility_in = [1, 2]
+        self.dirty_floor_posiblity = [1,2,3,4,5,6,7,8,9,0]
+        self.dirty_floor_posiblity_in = [1,2,3,4,5]
+        self.pdc = random.randint(0,11)
         self.live_or_death = ["L1", "L1", "L1", "L1", "D"]
-        self.state = 4
         self.sick_state = 3
         self.deathly_sick_state = 5
         self.death_state = 0
-        self.state_change = 1
-        self.pos = [self.x, self.y]
-        self.sick_possibility = 2
         self.deathly_sick_posibility = "healthy"
         self.live_death = "Live"
-        self.disease = False
+        self.dirty_floor_or_normal = [0, 6]
+        self.dirty_floor_in = 9
         self.deathly = False
-        self.duration_of_illnes = random.randint(10, 20)
+        self.disease = False
+        self.duration_of_illnes = random.randint(30, 50)
+        self.state = 4
+        self.sick_possibility = 0
     # hareketi tanımlayan fonksiyon
     def move(self):
         self.Life = True
         self.komsular = grid_bl.get_neighbours(self.x, self.y)
         gidecegi_yer = random.choice(self.komsular)
-        #komsu hucrelerinden birinin statei 2 den buyukse yani yakınlarında başka hucre varsa kendi stateini 1 azaltıyor
         #for i in self.komsular:
             #if grid_bl.get_xy(i[0], i[1]).state == 3 and self.state == 4:
                 #self.sick_possibility = random.choice(self.possibility)
-
-        if grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state == 3 and self.state == 4:
-            self.sick_possibility = random.choice(self.possibility)
-
-        #gidecegi yerde bir hücre varsa oraya gitmiyor
-        if grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state >= 2:
+        if grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state == 6 or grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state == 6:
+            if self.y >= -2:
+                self.dirty_floor_sick = random.choice(self.dirty_floor_posiblity_in)
+            elif self.y <= -2:
+                self.dirty_floor_sick = random.choice(self.dirty_floor_posiblity)
+            if self.dirty_floor_sick == 1:
+                print("zeminden bulastı")
+                self.sick_possibility = 1
+                self.dirty_floor_sick = 0
+            grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state = 0
+        if grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state == 3 or grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state == 5 and self.state == 4:
+            if self.y >= -2:
+                self.sick_possibility = random.choice(self.possibility_in)
+            elif self.y <= -2:
+                self.sick_possibility = random.choice(self.possibility)
             #self.state -= 1
             self.komsular.remove(gidecegi_yer)
             new_pos = random.choice(self.komsular)
@@ -121,92 +88,114 @@ class cell(object):
             self.x = new_pos[0]
             self.y = new_pos[1]
             self.komsular.append(gidecegi_yer)
+        #gidecegi yerde bir hücre varsa oraya gitmiyor
+        elif grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state == 2 and grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state == 3 and grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state == 4 and grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state == 5:
+            self.komsular.remove(gidecegi_yer)
+            new_pos = random.choice(self.komsular)
+            print("yasak konum silindi ve yeni konum seçildi")
+            #print("yeni konum", new_pos)
+            grid_bl.get_xy(new_pos[0], new_pos[1]).state = grid_bl.get_xy(self.x, self.y).state
+            grid_bl.get_xy(new_pos[0], new_pos[1]).state = self.state
+            if self.state == 4:
+                grid_bl.get_xy(self.x, self.y).state = 0
+            elif self.state == 3 or self.state == 5:
+                grid_bl.get_xy(self.x, self.y).state = random.choice(self.dirty_floor_or_normal)
+            self.x = new_pos[0]
+            self.y = new_pos[1]
+            self.komsular.append(gidecegi_yer)
 
         # state 2 den buyukse ve baska etken yoksa normal random hareket ediyor
         elif self.state >= 2:
             grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state = grid_bl.get_xy(self.x, self.y).state
             grid_bl.get_xy(gidecegi_yer[0], gidecegi_yer[1]).state = self.state
-            grid_bl.get_xy(self.x, self.y).state = 0
+            if self.state == 4:
+                grid_bl.get_xy(self.x, self.y).state = 0
+            elif self.state == 3 or self.state == 5:
+                if self.y <= -2:
+                    grid_bl.get_xy(self.x, self.y).state = random.choice(self.dirty_floor_or_normal)
+                elif self.y >= -2:
+                    grid_bl.get_xy(self.x, self.y).state = self.dirty_floor_in
             self.x = gidecegi_yer[0]
             self.y = gidecegi_yer[1]
         #eger hucrenin state i 2 ye esit olduysa hucreyi olduruyor
-        if grid_bl.get_xy(self.x, self.y).state == 2:
-            grid_bl.get_xy(self.x, self.y).state = self.death_state
-            self.Life = False
-            print("hücre öldü")
+
 
         if self.sick_possibility == 1:
             print("Hastalandı")
             self.disease = True
-            self.deathly_sick_posibility = random.choice(self.pdc)
-            if self.state == 4:
-                self.state = self.sick_state
-
-
-
-            if self.deathly_sick_posibility == "d":
-                print("ölümcül durumda")
-                self.deathly = True
-                self.state = self.deathly_sick_state
-                self.live_death = random.choice(self.live_or_death)
-                if self.live_death == "D":
-                    grid_bl.get_xy(self.x, self.y).state = self.death_state
-                    self.state = self.death_state
-                    self.Life = False
-                    print("hücre öldü")
+            self.state = self.sick_state
 
         if self.disease == True:
             self.duration_of_illnes -= 1
-            if self.duration_of_illnes == 0:
-                self.disease = False
-                #self.sick_possibility = 0
-                #self.deathly_sick_posibility = 0
-                self.state = 3
+            self.deathly_sick_posibility = random.randint(0,11)
+            print(self.deathly_sick_posibility)
+            print("hastalatıktaki gün",self.duration_of_illnes)
+            self.sick_possibility = 0
+
+            if self.duration_of_illnes <= 0:
+                self.sick_possibility = 0
+                self.deathly_sick_posibility = 0
+                self.state = 4
+                self.sick_possibility = 0
                 print("iyilesti")
+                self.disease = False
+                return self.duration_of_illnes
+        if self.deathly_sick_posibility == 5:
+            print("ölümcül durumda")
+            self.deathly = True
+            self.sick_possibility = 0
+            self.state = self.deathly_sick_state
+        if self.deathly == True:
+            self.live_death = random.choice(self.live_or_death)
+            if self.live_death == "D":
+                self.deathly_sick_posibility = 0
+                grid_bl.get_xy(self.x, self.y).state = self.death_state
+                self.state = 1
+                self.Life = False
+                print("hücre öldü")
 
-
-
-
-
-
-
-Viruscells = []
-vn = 3
-for i in range(vn):
-    Virus = viruscell()
-    Viruscells.append(Virus)
-
-
-
-
-#n tane hücre oluşturma
+turn_number = []
+n_cell = []
 cells = []
-n = 5
+n = 15
+sick_cell = 5
+#n tane hücre oluşturma
+
 for i in range(n):
-    Cell = cell()
-    cells.append(Cell)
+    if i < sick_cell:
+        Hex_Cell = cell()
+        cells.append(Hex_Cell)
+        cells[i].sick_possibility = 1
+    elif i >= sick_cell:
+        Hex_Cell = cell()
+        cells.append(Hex_Cell)
+
+#for i in range(sick_cell):
+    #cells[i].sick_possibility = 1
+
 
 #hücrelerin hareketini güncelliyor ve olen hucreleri siliyor
 Running = True
 turn = 0
-while Running:
+while Running == True:
     turn += 1
+    turn_number.append(turn)
+    a = len(cells)
+    n_cell.append(a)
+    if turn == 100:
+        break
     #print("tur",turn)
-    if len(cells) == 1:
-        print("Simulasyon", turn, ".turda tamamlandı")
+    #if len(cells) == 8:
         #break
     for i in cells:
         i.move()
-        #time.sleep(0.1)
         if i.Life == False:
             cells.remove(i)
 
-    for i in Viruscells:
-        i.movevirus()
-        #time.sleep(0.1)
-        if i.Lifevirus == False:
-            Viruscells.remove(i)
+
     grid_bl.visualize()
+
+
 
 
     #state i 1 e esıt olan tum hucrelerin stateini sıfırlıyor arka planı net görebilmek için yaptım
@@ -214,8 +203,24 @@ while Running:
         for j in ny_list:
             if grid_bl.get_xy(i, j).state == 1:
                 grid_bl.get_xy(i, j).state -= 1
-                print("şu konumda hasta hücre var",[i, j])
-                break
+                #print("şu konumda hasta hücre var", [i, j])
+
+            if grid_bl.get_xy(i, j).state == 6:
+                dirty_floor = random.randint(0, 20)
+                print("floor",dirty_floor)
+                if dirty_floor == 5:
+                    grid_bl.get_xy(i, j).state = 0
+                    dirty_floor = 0
+            if grid_bl.get_xy(i, j).state == 9:
+                dirty_floor = random.randint(0, 50)
+                print("floor",dirty_floor)
+                if dirty_floor == 5:
+                    grid_bl.get_xy(i, j).state = 0
+                    dirty_floor = 0
 
 
 
+plt.xlabel('Turn')
+plt.ylabel('Total Cell')
+plt.title('SAXS DATA3')
+plt.plot(turn_number, n_cell, color="Red")
